5e4ecbac58f35c8e424478176e02994d18376b49
Austin Oler <oleraustin@gmail.com>
2023-11-06 21:36:18 -0500

    added funtion to render search history, save to local storage, and updated results to display Fahrenheit

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var API_KEY = "YOUR_ad03ec6026a6a854547434fe2090eef0";
+var API_KEY = "ad03ec6026a6a854547434fe2090eef0"; 
 var apiUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
 var searchForm = document.getElementById("search-form");
@@ -9,10 +9,8 @@ var searchHistory = document.getElementById("search-history");
 
 // Function to fetch weather data from the OpenWeatherMap API
 function getWeatherData(city) {
-    // Construct the API URL with the city name and your API key
     var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
 
-    // Make the API request using the fetch API
     fetch(apiUrl)
         .then((response) => {
             if (!response.ok) {
@@ -21,70 +19,75 @@ function getWeatherData(city) {
             return response.json();
         })
         .then((data) => {
-            // Parse the response data and update the currentWeather and forecast sections
             displayWeatherData(data);
+            saveCityToLocalStorage(city); // Save the city to search history
         })
         .catch((error) => {
             console.error("Error fetching data:", error);
-            // You can handle and display an error message to the user here
+            // Display an error message to the user
+            currentWeather.innerHTML = "Error: Could not retrieve weather data.";
         });
 }
 
 // Function to display weather data in the UI
 function displayWeatherData(data) {
-    // Extract relevant data from the response and update the UI
-    // Example: Display city name, date, weather icon, temperature, humidity, and wind speed
-    var city = data.city.name;
-    var date = new Date(data.list[0].dt * 1000); // Convert timestamp to date
-    var weatherIcon = data.list[0].weather[0].icon;
-    var temperature = data.list[0].main.temp;
-    var humidity = data.list[0].main.humidity;
-    var windSpeed = data.list[0].wind.speed;
-
-    // Function to display weather data in the UI
-function displayWeatherData(data) {
     // Extract relevant data for the 5-day forecast
-    var forecastData = data.list.slice(1, 6); // Get the next 5 days
+    var forecastData = data.list;
 
     // Clear the forecast section
     forecast.innerHTML = "";
 
-    // Loop through the forecastData and create HTML elements for each day
+    var currentDate = null;
+
     forecastData.forEach((dayData) => {
         var date = new Date(dayData.dt * 1000).toLocaleDateString();
+        var temperatureKelvin = dayData.main.temp;
+        var temperatureFahrenheit = ((temperatureKelvin - 273.15) * 9/5) + 32;
         var weatherIcon = dayData.weather[0].icon;
-        var temperature = dayData.main.temp;
         var humidity = dayData.main.humidity;
         var windSpeed = dayData.wind.speed;
 
-        // Create a container for each day's data
-        const dayContainer = document.createElement("div");
-        dayContainer.classList.add("forecast-day");
+        // Check if it's a new date
+        if (date !== currentDate) {
+            // Create a container for each day's data
+            var dayContainer = document.createElement("div");
+            dayContainer.classList.add("forecast-day");
 
-        // Update the day's data
-        dayContainer.innerHTML = `
-            <h3>${date}</h3>
-            <img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
-            <p>Temperature: ${temperature}°C</p>
-            <p>Humidity: ${humidity}%</p>
-            <p>Wind Speed: ${windSpeed} m/s</p>
-        `;
+            dayContainer.innerHTML = `
+                <h3>${date}</h3>
+                <img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
+                <p>Temperature: ${temperatureFahrenheit.toFixed(2)}°F</p>
+                <p>Humidity: ${humidity}%</p>
+                <p>Wind Speed: ${windSpeed} m/s</p>
+            `;
 
-        // Append the day's container to the forecast section
-        forecast.appendChild(dayContainer);
+            // Append the day's container to the forecast section
+            forecast.appendChild(dayContainer);
+
+            currentDate = date;
+        }
     });
 }
 
-};
 // Function to save the city to localStorage
 function saveCityToLocalStorage(city) {
-    // Store the city in localStorage to maintain search history.
-    // Use an array to keep track of the searched cities and update the search history section.
+    var searchHistoryArray = JSON.parse(localStorage.getItem("searchHistory")) || [];
+    if (!searchHistoryArray.includes(city)) {
+        searchHistoryArray.push(city);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistoryArray));
+        renderSearchHistory();
+    }
 }
 
 // Function to render search history
 function renderSearchHistory() {
-    // Retrieve the search history from localStorage and display it in the search history section.
+    var searchHistoryArray = JSON.parse(localStorage.getItem("searchHistory")) || [];
+    searchHistory.innerHTML = "";
+    searchHistoryArray.forEach((city) => {
+        var li = document.createElement("li");
+        li.textContent = city;
+        searchHistory.appendChild(li);
+    });
 }
 
 // Event listener for the search form submission
@@ -93,8 +96,7 @@ searchForm.addEventListener("submit", function (e) {
     var city = cityInput.value.trim();
     if (city !== "") {
         getWeatherData(city);
-        saveCityToLocalStorage(city);
-        cityInput.value = ""; // Clear the input field after the search
+        cityInput.value = "";
     }
 });
 
