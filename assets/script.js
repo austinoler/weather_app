e61cd75fa262fbd0088a656bd82a6dccd6ca9307
Austin Oler <oleraustin@gmail.com>
2023-11-06 23:12:19 -0500

    added more styling and fixed bug that showed wrong current weather

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,20 @@ var currentWeather = document.getElementById("current-weather");
 var forecast = document.getElementById("forecast");
 var searchHistory = document.getElementById("search-history");
 
+// Function to get the day of the week from a Date object
+function getDayOfWeek(date) {
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return daysOfWeek[date.getUTCDay()];
+}
+
+// Function to format a Date object as "MM/DD/YYYY"
+function formatDate(date) {
+    var day = date.getUTCDate();
+    var month = date.getUTCMonth() + 1; // Months are 0-indexed, so add 1
+    var year = date.getUTCFullYear();
+    return `${month}/${day}/${year}`;
+}
+
 // Function to fetch weather data from the OpenWeatherMap API
 function getWeatherData(city) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
@@ -33,22 +47,41 @@ function getWeatherData(city) {
 // Function to display current weather data in the UI
 function displayCurrentWeather(data) {
     var city = data.city.name;
-    var date = new Date(data.list[0].dt * 1000).toLocaleDateString();
-    var temperatureKelvin = data.list[0].main.temp;
+    var currentWeatherData = data.list[0];
+    var temperatureKelvin = currentWeatherData.main.temp;
     var temperatureFahrenheit = ((temperatureKelvin - 273.15) * 9/5) + 32;
-    var weatherIcon = data.list[0].weather[0].icon;
-    var humidity = data.list[0].main.humidity;
-    var windSpeed = data.list[0].wind.speed;
+    var weatherIcon = currentWeatherData.weather[0].icon;
+    var humidity = currentWeatherData.main.humidity;
+    var windSpeed = currentWeatherData.wind.speed;
 
-    // Update the currentWeather section with current weather data
+    // Get the current date and time based on the user's time zone
+    var currentTime = new Date();
+    var formattedDate = formatDate(currentTime);
+    var localTime = formatTime(currentTime);
+
+    // Update the currentWeather section with current weather data and local date/time
     currentWeather.innerHTML = `
         <h2>Current Weather in ${city}</h2>
-        <p>Date: ${date}</p>
+        <p>Date: ${formattedDate}</p>
+        <p>Time: ${localTime}</p>
         <img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
         <p>Temperature: ${temperatureFahrenheit.toFixed(2)}°F</p>
         <p>Humidity: ${humidity}%</p>
         <p>Wind Speed: ${windSpeed} m/s</p>
     `;
+}
+
+function formatDate(date) {
+    var day = date.getDate();
+    var month = date.getMonth() + 1; // Months are 0-indexed, so add 1
+    var year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+}
+
+function formatTime(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    return `${hours}:${(minutes < 10 ? '0' : '')}${minutes}`;
 }
 
 // Function to display 5-day forecast weather data in the UI
@@ -58,31 +91,46 @@ function displayWeatherData(data) {
     // Clear the forecast section
     forecast.innerHTML = "";
 
+    // Add a title for the 5-day forecast
+    var forecastTitle = document.createElement("h2");
+    forecastTitle.textContent = "5 Day Forecast";
+    forecast.appendChild(forecastTitle);
+
     var currentDate = null;
 
     forecastData.forEach((dayData) => {
-        var date = new Date(dayData.dt * 1000).toLocaleDateString();
+        var timestamp = dayData.dt * 1000;
+        var date = new Date(timestamp);
+        var dayOfWeek = getDayOfWeek(date);
+        var formattedDate = formatDate(date);
         var temperatureKelvin = dayData.main.temp;
         var temperatureFahrenheit = ((temperatureKelvin - 273.15) * 9/5) + 32;
         var weatherIcon = dayData.weather[0].icon;
         var humidity = dayData.main.humidity;
         var windSpeed = dayData.wind.speed;
 
-        if (date !== currentDate) {
+        if (formattedDate !== currentDate) {
+            // Create a container for each day's data
             var dayContainer = document.createElement("div");
             dayContainer.classList.add("forecast-day");
 
-            dayContainer.innerHTML = `
-                <h3>${date}</h3>
+            // Create a title for the day (day of the week and date)
+            var dayTitle = document.createElement("h3");
+            dayTitle.textContent = `${dayOfWeek}, ${formattedDate}`;
+
+            dayContainer.appendChild(dayTitle);
+
+            dayContainer.innerHTML += `
                 <img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="Weather Icon">
                 <p>Temperature: ${temperatureFahrenheit.toFixed(2)}°F</p>
                 <p>Humidity: ${humidity}%</p>
                 <p>Wind Speed: ${windSpeed} m/s</p>
             `;
 
+            // Append the day's container to the forecast section
             forecast.appendChild(dayContainer);
 
-            currentDate = date;
+            currentDate = formattedDate;
         }
     });
 }
